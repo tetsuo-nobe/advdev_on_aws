2b3f55d5751ec97ee70a4e09f9abc045e8e22430
Tetsuo Nobe <tnobe@amazon.co.jp>
2022-03-19 12:50:17 +0900

    fixed appconfig.py

--- mod2/appconfig/appconfig.py
+++ mod2/appconfig/appconfig.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 import json
@@ -10,7 +10,7 @@ from boto3.session import Session
 import time
 
 
-# In[5]:
+# In[2]:
 
 
 # デモ用のプロファイルを使用
@@ -18,37 +18,57 @@ profile = 'advdev'
 session = Session(profile_name=profile)
 
 # 現在の設定値を取得・表示
-client = session.client('appconfig')
-response = client.get_configuration(
-            Application="DemoAppConfig",
-            Environment="prod",
-            Configuration="imageFile",
-            ClientId="client-999"
+client = session.client('appconfigdata')
+
+config_session= client.start_configuration_session(
+    ApplicationIdentifier='DemoAppConfig',
+    ConfigurationProfileIdentifier='imageFile',
+    EnvironmentIdentifier='prod',
+    RequiredMinimumPollIntervalInSeconds=15
 )
-config = response["Content"].read().decode('utf-8')
+
+response = client.get_latest_configuration(
+    ConfigurationToken=config_session['InitialConfigurationToken']
+)
+
+config = response["Configuration"].read().decode('utf-8')
 files = json.loads(config)
 
 for imageFile in files["imageFileList"]:
         print(imageFile["name"])
 
 
-# In[7]:
+# In[3]:
 
 
-# 10個のクライアントが繰り返し設定を取得する
-for cnt in range(1,21):
-    for i in range(0,10):
-        clientId = "client-" + str(i)
-        response = client.get_configuration(
-                    Application="DemoAppConfig",
-                    Environment="prod",
-                    Configuration="imageFile",
-                    ClientId=clientId
+# 10個のクライアントセッションを作成
+config_sessions = []
+for i in range(0,10):
+        config_session= client.start_configuration_session(
+            ApplicationIdentifier='DemoAppConfig',
+            ConfigurationProfileIdentifier='imageFile',
+            EnvironmentIdentifier='prod',
+            RequiredMinimumPollIntervalInSeconds=15
         )
-        config = response["Content"].read().decode('utf-8')
+        config_sessions.append(config_session)
+# 10個のクライアントセッションが繰り返し設定を取得する
+for cnt in range(1,13):
+    no = 0
+    for sess in config_sessions:
+        no += 1
+        clientId = "client-" + str(no)
+        response = client.get_latest_configuration(
+             ConfigurationToken=sess['InitialConfigurationToken']
+        )
+        config = response["Configuration"].read().decode('utf-8')
         files = json.loads(config)
         for imageFile in files["imageFileList"]:
                 print(clientId+": "+imageFile["name"])
-    time.sleep(3)
-    sec = 3 * cnt
+    time.sleep(5)
+    sec = 5 * cnt
     print("--- " + str(sec) + "秒経過 -----------------------------")
+
+
+
+
+
